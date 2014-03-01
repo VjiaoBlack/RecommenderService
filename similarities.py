9a8db4e5038adf5770c3e2714791fa6312bd4249
Freeman Lou <freemanlou2430@yahoo.com>
2014-03-01 10:51:23 -0500

    similarities funciton first draft

--- similarities.py
+++ similarities.py
@@ -4,20 +4,20 @@ import math
 list1 = [5,1,2,6,2]
 list2 = [1,3,5,0,3]
 list3 = [5,1,2,6,2]
-
+list4 = [99,99,99,99,99]
 
 def similarity(a,b):
-
-    zipped = zip(a,b)
-
-    for pair in zipped:	
-		d = math.sqrt(pair[0] ** 2 + pair[1] ** 2)
-		likeness += 1/(d+1)
-
-    return likeness
+	d = 0
+	zipped = zip(a,b)
+	
+	for x, y in zipped:
+		d += (x - y) ** 2
+	return  1/math.sqrt((d+1))
 
 print similarity(list1,list2)
 print '\n'
 print similarity(list1,list3)
+print '\n'
+print similarity(list1,list4)
 
 
