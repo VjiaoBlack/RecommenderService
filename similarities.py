f6e9e04d9625c93abda08b79db789be0c875f4c0
Freeman Lou <freemanlou2430@yahoo.com>
2014-03-01 10:57:04 -0500

    similarities function 2nd draft

--- similarities.py
+++ similarities.py
@@ -5,12 +5,15 @@ list1 = [5,1,2,6,2]
 list2 = [1,3,5,0,3]
 list3 = [5,1,2,6,2]
 list4 = [99,99,99,99,99]
+list5 = [5,-1,-2,-4,2]
 
 def similarity(a,b):
 	d = 0
 	zipped = zip(a,b)
 	
 	for x, y in zipped:
+		if x < 0 or y <0:
+			continue
 		d += (x - y) ** 2
 	return  1/math.sqrt((d+1))
 
@@ -19,5 +22,7 @@ print '\n'
 print similarity(list1,list3)
 print '\n'
 print similarity(list1,list4)
+print '\n'
+print similarity(list1,list5)
 
 
