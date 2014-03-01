d1a1f7255dc2f18a4cffbef0f3b98264a406b793
Freeman Lou <freemanlou2430@yahoo.com>
2014-03-01 11:37:57 -0500

    Linked app.py with similarites.py. Fixed app.py Flask linking

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import flask, render_template
+from flask import Flask, render_template, request
+
+from similarities import similarity
 
 app = Flask(__name__)
 
 @app.route('/')
 def normal():
-    return render_template("home.html")
+	return render_template("home.html")
 
 if __name__=="__main__":
-    app.debug = True
-    app.run(host="0.0.0.0",port=5000)
-
+	app.debug = True
+	app.run(host="0.0.0.0",port=5000)
--- similarities.py
+++ similarities.py
@@ -1,30 +1,28 @@
 import math
 
-#Test lists 5 dimension
+#Test 5 dimension lists
 list1 = [5,1,2,6,2]
 list2 = [1,3,5,0,3]
 list3 = [5,1,2,6,2]
 list4 = [99,99,99,99,99]
 list5 = [5,-1,-2,-4,2]
 
-
-
 def similarity(a,b):
 	distance = 0
 	zipped = zip(a,b)
-	
+
 	for x, y in zipped:
 		if x < 0 or y < 0:
 			continue
 		distance += (x - y) ** 2
 	return  1 / math.sqrt((distance+1))
 
-print similarity(list1,list2)
-print '\n'
-print similarity(list1,list3)
-print '\n'
-print similarity(list1,list4)
-print '\n'
-print similarity(list1,list5)
+#print similarity(list1,list2)
+#print '\n'
+#print similarity(list1,list3)
+#print '\n'
+#print similarity(list1,list4)
+#print '\n'
+#print similarity(list1,list5)
 
 
