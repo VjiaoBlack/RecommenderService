97071c084d3ad1dcb16c7737159235a1115d0f98
Victor Jiao <VjiaoBlack@gmail.com>
2014-03-01 10:23:42 -0500

    server done

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def normal():
+    return render_template("home.html")
+
+if __name__=="__main__":
+    app.debug = True
+    app.run(host="0.0.0.0",port=5000)
+
