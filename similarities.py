50f3533d9418b04b9bdd96e2e5199b65ca872e37
Victor Jiao <VjiaoBlack@gmail.com>
2014-03-01 10:36:28 -0500

    forced to switch 'drivers' lol

--- similarities.py
+++ similarities.py
@@ -3,11 +3,21 @@ import math
 #Test lists 5 dimension
 list1 = [5,1,2,6,2]
 list2 = [1,3,5,0,3]
+list3 = [5,1,2,6,2]
 
 
 def similarity(a,b):
-	for i in range(len()):	
-		d = sqrt()
-		return 1/(d+1)
+
+    zipped = zip(a,b)
+
+    for pair in zipped:	
+		d = math.sqrt(pair[0] ** 2 + pair[1] ** 2)
+		likeness += 1/(d+1)
+
+    return likeness
+
+print similarity(list1,list2)
+print '\n'
+print similarity(list1,list3)
 
 
