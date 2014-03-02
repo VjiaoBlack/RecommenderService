d65e77c4895a3ecfdd7f0f48b0c0dfacbb41261e
Victor Jiao <VjiaoBlack@gmail.com>
2014-03-02 09:23:38 -0500

    added recommender function that recommends who u should go with

--- similarities.py
+++ similarities.py
@@ -7,6 +7,8 @@ list3 = [5,1,2,6,2]
 list4 = [99,99,99,99,99]
 list5 = [5,-1,-2,-4,2]
 
+data = [list1, list2, list3, list4, list5]
+
 def similarity(a,b):
 	distance = 0
 	zipped = zip(a,b)
@@ -16,6 +18,23 @@ def similarity(a,b):
 			continue
 		distance += (x - y) ** 2
 	return  1 / math.sqrt((distance+1))
+
+def check(input):
+    i = 0
+    j = 0
+    ans = 0
+    for person in data:
+        j += 1
+        if similarity(person, input) > ans:
+            ans = similarity(person, input)
+            i = j
+
+    print "the best list is list" + str(i) + " with similarity " + str(ans)
+    return i
+
+check([5,1,2,6,2])
+
+check([90,90,90,90,90])
 
 #print similarity(list1,list2)
 #print '\n'
