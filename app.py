438f0a7d2f0f73d98a09db7e435720697e407b65
Victor Jiao <VjiaoBlack@gmail.com>
2014-03-29 11:36:22 -0400

    frontend is functional

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
 from flask import Flask, render_template, request
 
-from similarities import similarity
+from similarities import similarity, check, query
 
 app = Flask(__name__)
 
 @app.route('/')
-def normal():
+def default():
+    return render_template("intro.html")
+
+@app.route('/home')
+def home():
 	return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/results',methods=["post"])
+def results():
+    a = [-1,-1,-1,-1,-1]
+    a[0] = int(request.form['rating1'])
+    a[1] = int(request.form['rating2'])
+    a[2] = int(request.form['rating3'])
+    a[3] = int(request.form['rating4'])
+    a[4] = int(request.form['rating5'])
+
+    ans = query(a,5)
+
+    return render_template("results.html",query=5,ans=ans)
 
 if __name__=="__main__":
 	app.debug = True
--- similarities.py
+++ similarities.py
@@ -1,11 +1,11 @@
 import math
 
 #Test 5 dimension lists
-list1 = [5,1,2,6,2]
-list2 = [1,3,5,0,3]
-list3 = [5,1,2,6,2]
-list4 = [99,99,99,99,99]
-list5 = [5,-1,-2,-4,2]
+list1 = [5,1,2,6,2,1]
+list2 = [1,3,5,0,3,2]
+list3 = [5,1,2,6,2,3]
+list4 = [99,99,99,99,99,4]
+list5 = [5,-1,-2,-4,2,5]
 
 data = [list1, list2, list3, list4, list5]
 
@@ -19,7 +19,7 @@ def similarity(a,b):
 		distance += (x - y) ** 2
 	return  1 / math.sqrt((distance+1))
 
-def check(input):
+def check(input): #input is a list of length 'x'
     i = 0
     j = 0
     ans = 0
@@ -30,11 +30,23 @@ def check(input):
             i = j
 
     print "the best list is list" + str(i) + " with similarity " + str(ans)
-    return i
+    return {
+        1:list1,
+        2:list2,
+        3:list3,
+        4:list4,
+        5:list5
+    }[i]
 
-check([5,1,2,6,2])
 
-check([90,90,90,90,90])
+def query(input,query):
+    i = check(input) # i is the list with the best similarity
+    return i[query] # returns rating of the query from the one with closest similarity.
+
+
+# check([5,1,2,6,2])
+
+# check([90,90,90,90,90])
 
 #print similarity(list1,list2)
 #print '\n'
